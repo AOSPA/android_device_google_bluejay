AOSPA/android_device_google_raviole

Set TARGET_PREBUILT_KERNEL
Change-Id: Ia3ff828b201df2595486c91b6c27b0beadec631e

device-aospa.mk
@@ -26,6 +26,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
+
 # Properties
 TARGET_VENDOR_PROP := device/google/raviole/vendor.prop
 

AOSPA/android_device_google_bluejay

device-aospa.mk
@@ -25,6 +25,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES := \
     HbmSVManagerOverlay
 
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/bluejay-kernel/Image.lz4
+
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     system/bt/conf \